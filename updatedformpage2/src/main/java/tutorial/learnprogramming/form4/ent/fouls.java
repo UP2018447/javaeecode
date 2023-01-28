4f944b720883ca1fad18df0b28b5094df75c7fd1
Adam Thornton <91482685+UP2018447@users.noreply.github.com>
2023-01-28 19:24:01 +0000

    current form (working) (28/01/2023)

---
+++ updatedformpage2/src/main/java/tutorial/learnprogramming/form4/ent/fouls.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial.learnprogramming.form4.ent;
+
+import java.util.Map;
+
+/**
+ *
+ * @author adamt
+ */
+public class fouls {
+    
+    public Map<String,String> getFouls(Map<String,String> foulList){
+        foulList.put("APS", "APS");
+        foulList.put("ATR", "ATR");
+        foulList.put("BAT", "BAT");
+        foulList.put("DEH", "DEH");
+        foulList.put("DOD", "DOD");
+        foulList.put("DOF", "DOF");
+        foulList.put("DOG", "DOG");
+        foulList.put("DPI", "DPI");
+        foulList.put("DSH", "DSH");
+        foulList.put("DSQ", "DSQ");
+        foulList.put("ENC", "ENC");
+        foulList.put("FGT", "FGT");
+        foulList.put("FST", "FST");
+        foulList.put("IBB", "IBB");
+        foulList.put("IBK", "IBK");
+        foulList.put("IBP", "IBP");
+        foulList.put("IDP", "IDP");
+        foulList.put("IFD", "IFD");
+        foulList.put("IFH", "IFH");
+        foulList.put("IFK", "IFK");
+        foulList.put("IFP", "IFP");
+        foulList.put("IJY", "IJY");
+        foulList.put("IKB", "IKB");
+        foulList.put("ILF", "ILF");
+        foulList.put("ILM", "ILM");
+        foulList.put("ING", "ING");
+        foulList.put("IPN", "IPN");
+        foulList.put("IPR", "IPR");
+        foulList.put("ISH", "ISH");
+        foulList.put("ISP", "ISP");
+        foulList.put("ITP", "ITP");
+        foulList.put("IUH", "IUH");
+        foulList.put("IWK", "IWK");
+        foulList.put("KCI", "KCI");
+        foulList.put("KIK", "KIK");
+        foulList.put("KOB", "KOB");
+        foulList.put("OBK", "OBK");
+        foulList.put("OFH", "OFH");
+        foulList.put("OFK", "OFK");
+        foulList.put("OPI", "OPI");
+        foulList.put("PF-BBW", "PF-BBW");
+        foulList.put("PF-BOB", "PF-BOB");
+        foulList.put("PF-BTH", "PF-BTH");
+        foulList.put("PF-CHB", "PF-CHB");
+        foulList.put("PF-CLP", "PF-CLP");
+        foulList.put("PF-FMM", "PF-FMM");
+        foulList.put("PF-HCT", "PF-HCT");
+        foulList.put("PF-HDR", "PF-HDR");
+        foulList.put("PF-HTF", "PF-HTF");
+        foulList.put("PF-HUR", "PF-HUR");
+        foulList.put("PF-ICS", "PF-ICS");
+        foulList.put("PF-LEA", "PF-LEA");
+        foulList.put("PF-LHP", "PF-LHP");
+        foulList.put("PF-LTO", "PF-LTO");
+        foulList.put("PF-OTH", "PF-OTH");
+        foulList.put("PF-RFK", "PF-RFK");
+        foulList.put("PF-RTH", "PF-RTH");
+        foulList.put("PF-RTK", "PF-RTK");
+        foulList.put("PF-RTP", "PF-RTP");
+        foulList.put("PF-SKE", "PF-SKE");
+        foulList.put("PF-TGT", "PF-TGT");
+        foulList.put("PF-TRP", "PF-TRP");
+        foulList.put("PF-UNR", "PF-UNR");
+        foulList.put("RNH", "RNH");
+        foulList.put("RNK", "RNK");
+        foulList.put("SLI", "SLI");
+        foulList.put("SLM", "SLM");
+        foulList.put("SLW", "SLW");
+        foulList.put("SUB", "SUB");
+        foulList.put("UC-ABL", "UC-ABL");
+        foulList.put("UC-BCH", "UC-BCH");
+        foulList.put("UC-DEA", "UC-DEA");
+        foulList.put("UC-FCO", "UC-FCO");
+        foulList.put("UC-RHT", "UC-RHT");
+        foulList.put("UC-SBR", "UC-SBR");
+        foulList.put("UC-STB", "UC-STB");
+        foulList.put("UC-TAU", "UC-TAU");
+        foulList.put("UC-UNS", "UC-UNS");
+        foulList.put("UFA", "UFA");
+        foulList.put("UFT", "UFT");
+        return foulList;
+    }
+    
+}
