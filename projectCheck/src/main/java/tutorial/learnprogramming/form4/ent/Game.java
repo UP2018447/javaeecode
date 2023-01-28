d6a33026ad078f95a3c7d5e074f4b20961a17ade
Adam Thornton <91482685+UP2018447@users.noreply.github.com>
2023-01-28 18:13:08 +0000

    form (working) (28/01/2023)

---
+++ projectCheck/src/main/java/tutorial/learnprogramming/form4/ent/Game.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial.learnprogramming.form4.ent;
+
+import java.util.List;
+
+/**
+ *
+ * @author adamt
+ */
+public class Game {
+    
+    private List<Foul> foulList;
+    
+}
---
+++ projectCheck/src/main/java/tutorial/learnprogramming/form4/ent/foulRecord.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial.learnprogramming.form4.ent;
+
+/**
+ *
+ * @author adamt
+ */
+public class foulRecord {
+    
+    private final String quarter;
+    private final String time;
+    private final String team;
+    private final String foul;
+    private final String player;
+    private final String position;
+    private final String official;
+    private final String decision;
+
+    public foulRecord(String quarter, String time, String team, String foul, String player, String position, String official, String decision) {
+        this.quarter = quarter;
+        this.time = time;
+        this.team = team;
+        this.foul = foul;
+        this.player = player;
+        this.position = position;
+        this.official = official;
+        this.decision = decision;
+    }
+
+    public String getQuarter() {
+        return quarter;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getFoul() {
+        return foul;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getOfficial() {
+        return official;
+    }
+
+    public String getDecision() {
+        return decision;
+    }
+    
+    
+    
+}
