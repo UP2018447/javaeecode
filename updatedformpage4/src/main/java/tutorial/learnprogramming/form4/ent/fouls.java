5070c25af75680510f4e5f318fd26b9c8739a704
Adam Thornton <91482685+UP2018447@users.noreply.github.com>
2023-02-10 17:36:22 +0000

    updated form 4 (currently working) (10/02/2023)

---
+++ updatedformpage4/src/main/java/tutorial/learnprogramming/form4/ent/fouls.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial.learnprogramming.form4.ent;
+
+import java.util.Map;
+
+/**
+ *
+ * @author adamt
+ */
+public class fouls {
+    
+    public Map<String,String> getFouls(Map<String,String> foulList){
+        foulList.put("APS", "Altering playing surface");
+        foulList.put("ATR", "Assisting the runner");
+        foulList.put("BAT", "Illegal batting");
+        foulList.put("DEH", "Holding, defense");
+        foulList.put("DOD", "Delay of game, defense");
+        foulList.put("DOF", "Offside, defense");
+        foulList.put("DOG", "Delay of game, offense");
+        foulList.put("DPI", "Pass interference, defense");
+        foulList.put("DSH", "Delay of game, start of half");
+        foulList.put("DSQ", "Disqualification");
+        foulList.put("ENC", "Encroachment (offense)");
+        foulList.put("FGT", "Fighting");
+        foulList.put("FST", "False start");
+        foulList.put("IBB", "Illegal block in the back");
+        foulList.put("IBK", "Illegal block during kick");
+        foulList.put("IBP", "Illegal backward pass");
+        foulList.put("IDP", "Ineligible downfield on pass");
+        foulList.put("IFD", "Illegal formation, defense (3-on-1)");
+        foulList.put("IFH", "Illegal forward handling");
+        foulList.put("IFK", "Illegal free kick formation");
+        foulList.put("IFP", "Illegal forward pass");
+        foulList.put("IJY", "Illegal jerseys");
+        foulList.put("IKB", "Illegally kicking ball");
+        foulList.put("ILF", "Illegal formation");
+        foulList.put("ILM", "Illegal motion");
+        foulList.put("ING", "Intentional grounding");
+        foulList.put("IPN", "Improper numbering");
+        foulList.put("IPR", "Illegal procedure");
+        foulList.put("ISH", "Illegal shift");
+        foulList.put("ISP", "Illegal snap");
+        foulList.put("ITP", "Illegal touching of a forward pass");
+        foulList.put("IUH", "Illegal use of hands");
+        foulList.put("IWK", "Illegal wedge on kickoff");
+        foulList.put("KCI", "Kick-catch interference");
+        foulList.put("KIK", "Illegal kick");
+        foulList.put("KOB", "Free kick out of bounds");
+        foulList.put("OBK", "Out of bounds during kick");
+        foulList.put("OFH", "Holding, offense");
+        foulList.put("OFK", "Offside, free kick");
+        foulList.put("OPI", "Pass interference, offense");
+        foulList.put("PF-BBW", "Personal Foul, Blocking Below the Waist");
+        foulList.put("PF-BOB", "Personal Foul, Blocking Out of Bounds");
+        foulList.put("PF-BTH", "Personal Foul, Blow To the Head");
+        foulList.put("PF-CHB", "Personal Foul, Chop Block");
+        foulList.put("PF-CLP", "Personal Foul, Clipping");
+        foulList.put("PF-FMM", "Personal Foul, Face Mask");
+        foulList.put("PF-HCT", "Personal Foul, Horse Collar Tackle");
+        foulList.put("PF-HDR", "Personal Foul, Hit on Defenseless Receiver");
+        foulList.put("PF-HTF", "Personal Foul, Hands To the Face");
+        foulList.put("PF-HUR", "Personal Foul, Hurdling");
+        foulList.put("PF-ICS", "Personal Foul, Illegal Contact with Snapper");
+        foulList.put("PF-LEA", "Personal Foul, Leaping");
+        foulList.put("PF-LHP", "Personal Foul, Late Hit/Piling on");
+        foulList.put("PF-LTO", "Personal Foul, Late Hit out of Bounds");
+        foulList.put("PF-OTH", "Personal Foul, Other");
+        foulList.put("PF-RFK", "Personal Foul, Roughing Free Kicker");
+        foulList.put("PF-RTH", "Personal Foul, Roughing The Holder");
+        foulList.put("PF-RTK", "Personal Foul, Roughing The Kicker");
+        foulList.put("PF-RTP", "Personal Foul, Roughing The Passer");
+        foulList.put("PF-SKE", "Personal Foul, Striking/Kneeing/Elbowing");
+        foulList.put("PF-TGT", "Personal Foul, Targeting");
+        foulList.put("PF-TRP", "Personal Foul, Tripping");
+        foulList.put("PF-UNR", "Personal Foul, Unnecessary Roughness");
+        foulList.put("RNH", "Running into the holder");
+        foulList.put("RNK", "Running into the kicker");
+        foulList.put("SLI", "Sideline interference, 5 yards");
+        foulList.put("SLM", "Sideline interference, 15 yards");
+        foulList.put("SLW", "Sideline interference, warning");
+        foulList.put("SUB", "Illegal substitution");
+        foulList.put("UC-2PN", "Unsportsmanlike conduct, two players with same number");
+        foulList.put("UC-ABL", "Unsportsmanlike conduct, abusive language");
+        foulList.put("UC-BCH", "Unsportsmanlike conduct, bench");
+        foulList.put("UC-DEA", "Unsportsmanlike conduct, delayed/excessive act");
+        foulList.put("UC-FCO", "Unsportsmanlike conduct, forcibly contacting an official");
+        foulList.put("UC-RHT", "Unsportsmanlike conduct, removal of helmet");
+        foulList.put("UC-SBR", "Unsportsmanlike conduct, simulating being roughed");
+        foulList.put("UC-STB", "Unsportsmanlike conduct, spiking/throwing ball");
+        foulList.put("UC-TAU", "Unsportsmanlike conduct, taunting/baiting");
+        foulList.put("UC-UNS", "Unsportsmanlike conduct, other");
+        foulList.put("UFA", "Unfair acts");
+        foulList.put("UFT", "Unfair tactics");
+        return foulList;
+    }
+    
+}
